fdf2e5136b04d24d52d4bd829d3efd9614941c4f
toptaab <top_giveheart@hotmail.com>
2024-04-10 10:28:16 +0700

    add redux and authme axios check user condition

--- src/lib/store.ts
+++ src/lib/store.ts
@@ -1,10 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit'
 import counterReducer from './features/counter/counterSlice'
+import authReducer from './features/auth/authSlice'
 
 export const makeStore = () => {
   return configureStore({
     reducer: {
-      counter: counterReducer
+      counter: counterReducer,
+      auth: authReducer,
+
     }
   })
 }
